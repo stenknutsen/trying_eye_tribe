7828cb04754b240c19942e5e93ea09ed6f8f2840
stenknutsen <stenknutsen@gmail.com>
2016-05-18 15:29:21 -0400

    added timestamp

--- src/TETSimple.java
+++ src/TETSimple.java
@@ -1,4 +1,7 @@
 import com.theeyetribe.clientsdk.data.GazeData;
+
+import java.security.Timestamp;
+
 import com.theeyetribe.clientsdk.GazeManager;
 import com.theeyetribe.clientsdk.IGazeListener;
 import com.theeyetribe.clientsdk.data.CalibrationResult;
@@ -9,7 +12,8 @@ public class TETSimple {
 	static int gy;
 	static int hx;
 	static int hy;
-
+	static String time_now;
+	
 	public static void main(String[] args) {
 		System.out.println("Hi eyetribe!");
 		final GazeManager gm = GazeManager.getInstance();
@@ -20,11 +24,12 @@ public class TETSimple {
 
 			@Override
 			public void onGazeUpdate(GazeData gazeData) {
+				time_now = gazeData.timeStampString;
 				gx = (int) gazeData.leftEye.smoothedCoordinates.x;
 				gy = (int) gazeData.leftEye.smoothedCoordinates.y;
 				hx = (int) gazeData.rightEye.smoothedCoordinates.x;
 				hy = (int) gazeData.rightEye.smoothedCoordinates.y;
-				System.out.println(gx+ " , " + gy + " --- "+ hx + " , "+hy);
+				System.out.println(gx+ " , " + gy + " --- "+ hx + " , "+hy+", time:"+time_now);
 
 			}
 		});
@@ -37,13 +42,10 @@ public class TETSimple {
             public void run()
             {
                 gm.removeGazeListener(null);
-             gm.deactivate();
+                gm.deactivate();
             }
         });
 
 	}
 
 }
-
-
-
